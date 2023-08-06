e8ee25f326cb84b5ee29a02aa85f64b86840b32b
vm21242124 <vm21242124@gmail.com>
2023-08-06 12:28:52 +0530

    user route

--- Routes/User.route.js
+++ Routes/User.route.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { signUp, signin, signout } from '../Controllers/user.controller.js';
-import { IsLoggedIn } from '../Middleware/Auth.middleware.js';
+
 
 const router=express.Router()
 router.post('/register',signUp)
-router.post('/signin',IsLoggedIn,signin)
+router.post('/signin',signin)
 router.get('/signout',signout)
 
 export default router;
